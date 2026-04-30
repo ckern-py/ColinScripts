f49c83430610f1ee9199a8d2aff8ac6785f74fbd
ckern-py <48026725+ckern-py@users.noreply.github.com>
2026-04-29 19:27:58 -0500

    Enhance Jellyfin inventory script for movie types
    Added functionality to inventory Bluray and UHD movies, and log the results.

--- Bash/JellyfinInventory.sh
+++ Bash/JellyfinInventory.sh
@@ -2,9 +2,10 @@
 
 todaydate=$(date '+%Y%m%d')
 backuplog="/central/location/scripts/logs/JellyfinInventory/${todaydate}_JellyfinInventory.log"
+sambashare="/SSD_1/samba/share/Jellyfin/"
+moviesfile="/central/location/inventory/Jellyfin_Movies.txt"
 
 echo "$todaydate $(date +%T) Taking Jellyfin inventory" >> "$backuplog"
-
 #-------------------------------------------------------------
 echo "Taking Jellyfin inventory of shows" >> "$backuplog"
 readarray -t tv_array < <(ls /NVME_SSD/jellyfin/shows)
@@ -14,16 +15,27 @@ readarray -t -O "${#tv_array[@]}" tv_array < <(ls /HDD_1/jellyfin/shows)
 printf '%s\n' "${tv_array[@]}" | sort > /central/location/inventory/Jellyfin_Shows.txt
 #-------------------------------------------------------------
 echo "Taking Jellyfin inventory of movies" >> "$backuplog"
-readarray -t movie_array < <(ls /NVME_SSD/jellyfin/movies)
 
-readarray -t -O "${#movie_array[@]}" movie_array < <(ls /HDD_1/jellyfin/movies)
+readarray -t bluray_movie_array < <(ls /HDD_1/jellyfin/movies/bluray)
+echo "Bluray Movies:" > "$moviesfile"
+printf '%s\n' "${bluray_movie_array[@]}" | sort >> "$moviesfile"
 
-printf '%s\n' "${movie_array[@]}" | sort > /central/location/inventory/Jellyfin_Movies.txt
+readarray -t uhd_movie_array < <(ls /HDD_2/jellyfin/movies/uhd)
+echo "" >> "$moviesfile"
+echo "4K UHD Movies:" >> "$moviesfile"
+printf '%s\n' "${uhd_movie_array[@]}" | sort >> "$moviesfile"
 #-------------------------------------------------------------
 echo "Taking Jellyfin inventory of music" >> "$backuplog"
 readarray -t music_array < <(tree -d /NVME_SSD/jellyfin/music)
 
 printf '%s\n' "${music_array[@]}" > /central/location/inventory/Jellyfin_Music.txt
+#-------------------------------------------------------------
+if [ ! -d "$sambashare" ]
+then
+   mkdir "$sambashare"
+fi
+
+cp /central/location/inventory/* "$sambashare"
 #-------------------------------------------------------------
 echo "$todaydate $(date +%T) Finished taking Jellyfin inventory" >> "$backuplog"
 
